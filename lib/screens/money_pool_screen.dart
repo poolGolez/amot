319e49ef4586d4ce013bfeb233c84640e6ac148c
pgolez <paul.golez@codingavenue.com>
2020-08-01 20:30:19 +0800

    Initial screen for pooling money

---
+++ lib/screens/money_pool_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoneyPoolScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Samgyupsal'),
+      ),
+      body: Center(
+        child: Text('content here'),
+      ),
+    );
+  }
+}
