ebef005b002c030be2eac4acbfbd8982e7564b3a
pgolez <paul.golez@codingavenue.com>
2020-08-06 15:09:06 +0800

    Add toggle button for different views

--- lib/screens/current_portfolio_screen.dart
+++ lib/screens/current_portfolio_screen.dart
@@ -1,3 +1,4 @@
+import 'package:amot/models/portfolio.dart';
 import 'package:amot/screens/add_expense_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -8,7 +9,16 @@ import '../widgets/current_portfolio_screen/portfolio_header.dart';
 
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
-class CurrentPortfolioScreen extends StatelessWidget {
+enum PortfolioView { PARTAKER, EXPENSE }
+
+class CurrentPortfolioScreen extends StatefulWidget {
+  @override
+  _CurrentPortfolioScreenState createState() => _CurrentPortfolioScreenState();
+}
+
+class _CurrentPortfolioScreenState extends State<CurrentPortfolioScreen> {
+  PortfolioView view = PortfolioView.PARTAKER;
+
   @override
   Widget build(BuildContext context) {
     final portfolio = Provider.of<CurrentPortfolio>(context);
@@ -30,6 +40,44 @@ class CurrentPortfolioScreen extends StatelessWidget {
               color: Colors.white,
               child: PortfolioHeader(portfolio),
             ),
+            Container(
+              height: 40,
+              padding: const EdgeInsets.all(5.0),
+              child: Row(children: <Widget>[
+                RaisedButton(
+                  onPressed: view == PortfolioView.PARTAKER
+                      ? null
+                      : () => setState(() => view = PortfolioView.PARTAKER),
+                  child: Text('Partakers'),
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.horizontal(left: Radius.circular(5)),
+                  ),
+                  elevation: 10,
+                  color: Colors.white,
+                  textColor: Theme.of(context).primaryColor,
+                  disabledElevation: 0,
+                  disabledColor: Theme.of(context).primaryColor,
+                  disabledTextColor: Colors.white,
+                ),
+                RaisedButton(
+                  onPressed: view == PortfolioView.EXPENSE
+                      ? null
+                      : () => setState(() => view = PortfolioView.EXPENSE),
+                  child: Text('Expenses'),
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.horizontal(right: Radius.circular(5)),
+                  ),
+                  elevation: 6,
+                  color: Colors.white,
+                  textColor: Theme.of(context).primaryColor,
+                  disabledElevation: 0,
+                  disabledColor: Theme.of(context).primaryColor,
+                  disabledTextColor: Colors.white,
+                )
+              ]),
+            ),
             Expanded(
               child: PortfolioDivisionWidget(portfolio.division),
             ),
@@ -41,11 +89,14 @@ class CurrentPortfolioScreen extends StatelessWidget {
         child: Icon(Icons.add),
         children: <SpeedDialChild>[
           SpeedDialChild(
-              backgroundColor: Theme.of(context).accentColor,
-              child: Icon(Icons.receipt),
-              onTap: () => promptAddExpenseDialog(context)),
+            backgroundColor: Theme.of(context).accentColor,
+            child: Icon(Icons.receipt),
+            label: "Expense",
+            onTap: () => promptAddExpenseDialog(context),
+          ),
           SpeedDialChild(
             backgroundColor: Theme.of(context).accentColor,
+            label: "Participant",
             child: Icon(Icons.person),
           )
         ],
